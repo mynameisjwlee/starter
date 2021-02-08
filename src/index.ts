2aace149811a415fc91528465ebb2bd61cd25d20
mynameisjwlee <jinwook433@gmail.com>
2021-02-09 01:04:24 +0900

    husky setting

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import express from 'express';
 import { config as configEnv } from 'dotenv';
+import express from 'express';
 
 function startServer() {
   const app = express();
   configEnv();
-  
-  app.use(express.json());
 
+  app.use(express.json());
 
   app.listen(process.env.PORT, () => {
     console.log('server started...');
